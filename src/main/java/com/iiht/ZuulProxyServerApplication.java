f1da8231add404751d8679d252d67def639d040b
nikhilrajput911 <46365841+nikhilrajput911@users.noreply.github.com>
2022-08-28 13:47:44 +0530

    Added zuul filters

---
+++ src/main/java/com/iiht/ZuulProxyServerApplication.java
@@ -0,0 +1,41 @@
+package com.iiht;
+
+import com.iiht.zuul.filter.ErrorFilter;
+import com.iiht.zuul.filter.PostFilter;
+import com.iiht.zuul.filter.PreFilter;
+import com.iiht.zuul.filter.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyServerApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
